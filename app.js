e6cf75a3d9bb66eef96bb6c2100a2717e096a900
agastsya <agastsya007@gmail.com>
2023-09-17 03:06:17 +0530

    Added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { ConnectDB } from "./data/database.js";
 import { User } from "./models/user.js";
 import userRouter from "./routes/userRouter.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import express from "express";
 const app = express();
@@ -11,6 +12,13 @@ const router = express.Router();
 //MIDDLEWARES
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/api/v1/user", userRouter);
 
